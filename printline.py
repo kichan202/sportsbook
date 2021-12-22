6b6e01b443b90bf089c7f4cad44d4275926f92ed
Francisco <franciscodorset@hotmail.com>
2021-12-21 21:53:08 -0400

    change the print line function to skip games who's time has already passed

--- printline.py
+++ printline.py
@@ -26,15 +26,20 @@ def print_line():
     games = load_games()
     
     for key in games:
+        #must use variables
         main_list = games[key]
         vs_list = codes[main_list[7]]
         game_time = main_list[6]
 
         #string to time object
-        #time_object = datetime.strptime(game_time, '%H:%M:%S').time()
-        print("%s" %game_time)
-        spread_number = ""
-        game_string = "%s %s\t%s(%s)\t%s\t%s" %(key,codes[key][1], main_list[0], main_list[1], main_list[2], main_list[4])
-        vs_string = "%s %s\t%s\t\t%s\t%s" %(main_list[7], vs_list[1],main_list[8],main_list[9],main_list[5])
-        print("%s\n%s" %(game_string,vs_string))
+        time_object = datetime.strptime(game_time, '%I:%M %p').time()
+        #only games that the time hasn't passed will be available for now
+        if time_object > datetime.now().time():
+            print("%s" %game_time)
+            spread_number = ""
+            game_string = "%s %s\t%s(%s)\t%s\t%s" %(key,codes[key][1], main_list[0], main_list[1], main_list[2], main_list[4])
+            vs_string = "%s %s\t%s\t\t%s\t%s" %(main_list[7], vs_list[1],main_list[8],main_list[9],main_list[5])
+            print("%s\n%s" %(game_string,vs_string))
+        
+
 print_line()
