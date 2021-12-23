d3e78debcaf5514b24b835afbaea9777f1d5c16d
Francisco <franciscodorset@hotmail.com>
2021-12-22 22:54:50 -0400

    added all the printing code from the test file to the printline file

--- printline.py
+++ printline.py
@@ -30,11 +30,22 @@ def print_line(*argv):
     lpr = subprocess.Popen("/usr/bin/lpr", stdin=subprocess.PIPE)
     lpr.stdin.write(line.encode())
 
+def print_line(*argv):
+    time_object= datetime.now()
+    time = time_object.strftime("%d/%b/%Y %I:%M %p")
+    header ="%s\n\tFull Games\n\tTeams MoneyLine O/U"%time
+    line = "\t%s\n"%(header)
+    for arg in argv:
+        line+=arg
+    lpr = subprocess.Popen("/usr/bin/lpr", stdin=subprocess.PIPE)
+    lpr.stdin.write(line.encode())
 
-def nfl_line():
+
+def select_line(which_league):
     codes = load_codes()
     games = load_games()
-    title = "NFL: \t\tFULL TIME GAME"
+    has_a_game = 0
+    title = "%s: \t\tFULL TIME GAME"%str.upper(which_league)
     code_team = "CODE  TEAM\tSPREAD\t\tM.L\tO\\U"
     equals_sign="============================================"
     dash_sign = "--------------------------------------------"
@@ -46,7 +57,8 @@ def nfl_line():
             vs_list = codes[main_list[7]]
             game_time = main_list[6]
             league =codes[key][0]
-            if league == 'nfl':
+            if league == which_league:
+                has_a_game+=1
                 away_team_name =codes[key][1]
                 home_team_name = vs_list[1]
                 away_team_code = key
@@ -61,6 +73,10 @@ def nfl_line():
 
                 if away_team_spread > 0 :
                     away_team_spread = "+%s"%away_team_spread
+
+                #PickEm
+                if away_team_spread == 0 :
+                    away_team_spread = "PK"
                 
                 if home_team_money_line > 0 :
                     home_team_money_line = "+%s"%home_team_money_line
@@ -74,142 +90,9 @@ def nfl_line():
                     vs_string = "%s %s\t%s\t\t%s\t%s" %(home_team_code, home_team_name,home_team_spread_odds,home_team_money_line,over_under_odds)
 
                     data+="%s\n%s\n%s\n%s\n"%(game_time,game_string,vs_string,dash_sign)
-    return data
+    
+    if has_a_game>0:
+        return data
+    else:
+        return ""
 
-
-def nba_line():
-    codes = load_codes()
-    games = load_games()
-    title = "NBA: \t\tFULL TIME GAME"
-    code_team = "CODE  TEAM\tSPREAD\t\tM.L\tO\\U"
-    equals_sign="============================================"
-    dash_sign = "--------------------------------------------"
-    data="%s\n%s\n%s\n%s\n%s\n"%(equals_sign,title,equals_sign,code_team,dash_sign)
-    if bool(games):
-        for key in games:
-            #must use variables
-            main_list = games[key]
-            vs_list = codes[main_list[7]]
-            game_time = main_list[6]
-            league =codes[key][0]
-            if league == 'nba':
-                away_team_name =codes[key][1]
-                home_team_name = vs_list[1]
-                away_team_code = key
-                home_team_code=main_list[7]
-                away_team_spread=main_list[0]
-                away_team_spread_odds = main_list[1]
-                home_team_spread_odds= main_list[8]
-                over_under=main_list[4]
-                over_under_odds= main_list[5]
-                away_team_money_line = main_list[2]
-                home_team_money_line = main_list[9]
-
-                if away_team_spread > 0 :
-                    away_team_spread = "+%s"%away_team_spread
-                
-                if home_team_money_line > 0 :
-                    home_team_money_line = "+%s"%home_team_money_line
-
-                #string to time object
-                time_object = datetime.strptime(game_time, '%I:%M %p').time()
-                #only games that the time hasn't passed will be available for now
-                if time_object > datetime.now().time():
-                    #print("%s" %game_time)
-                    game_string = "%s %s\t%s(%s)\t%s\t%s" %(away_team_code,away_team_name, away_team_spread,away_team_spread_odds, away_team_money_line, over_under)
-                    vs_string = "%s %s\t%s\t\t%s\t%s" %(home_team_code, home_team_name,home_team_spread_odds,home_team_money_line,over_under_odds)
-
-                    data+="%s\n%s\n%s\n%s\n"%(game_time,game_string,vs_string,dash_sign)
-    return data
-
-
-def mlb_line():
-    codes = load_codes()
-    games = load_games()
-    title = "MLB: \t\tFULL TIME GAME"
-    code_team = "CODE  TEAM\tSPREAD\t\tM.L\tO\\U"
-    equals_sign="============================================"
-    dash_sign = "--------------------------------------------"
-    data="%s\n%s\n%s\n%s\n%s\n"%(equals_sign,title,equals_sign,code_team,dash_sign)
-    if bool(games):
-        for key in games:
-            #must use variables
-            main_list = games[key]
-            vs_list = codes[main_list[7]]
-            game_time = main_list[6]
-            league =codes[key][0]
-            if league == 'mlb':
-                away_team_name =codes[key][1]
-                home_team_name = vs_list[1]
-                away_team_code = key
-                home_team_code=main_list[7]
-                away_team_spread=main_list[0]
-                away_team_spread_odds = main_list[1]
-                home_team_spread_odds= main_list[8]
-                over_under=main_list[4]
-                over_under_odds= main_list[5]
-                away_team_money_line = main_list[2]
-                home_team_money_line = main_list[9]
-
-                if away_team_spread > 0 :
-                    away_team_spread = "+%s"%away_team_spread
-                
-                if home_team_money_line > 0 :
-                    home_team_money_line = "+%s"%home_team_money_line
-
-                #string to time object
-                time_object = datetime.strptime(game_time, '%I:%M %p').time()
-                #only games that the time hasn't passed will be available for now
-                if time_object > datetime.now().time():
-                    #print("%s" %game_time)
-                    game_string = "%s %s\t%s(%s)\t%s\t%s" %(away_team_code,away_team_name, away_team_spread,away_team_spread_odds, away_team_money_line, over_under)
-                    vs_string = "%s %s\t%s\t\t%s\t%s" %(home_team_code, home_team_name,home_team_spread_odds,home_team_money_line,over_under_odds)
-
-                    data+="%s\n%s\n%s\n%s\n"%(game_time,game_string,vs_string,dash_sign)
-    return data
-
-
-def nhl_line():
-    codes = load_codes()
-    games = load_games()
-    title = "NHL: \t\tFULL TIME GAME"
-    code_team = "CODE  TEAM\tSPREAD\t\tM.L\tO\\U"
-    equals_sign="============================================"
-    dash_sign = "--------------------------------------------"
-    data="%s\n%s\n%s\n%s\n%s\n"%(equals_sign,title,equals_sign,code_team,dash_sign)
-    if bool(games):
-        for key in games:
-            #must use variables
-            main_list = games[key]
-            vs_list = codes[main_list[7]]
-            game_time = main_list[6]
-            league =codes[key][0]
-            if league == 'NHL':
-                away_team_name =codes[key][1]
-                home_team_name = vs_list[1]
-                away_team_code = key
-                home_team_code=main_list[7]
-                away_team_spread=main_list[0]
-                away_team_spread_odds = main_list[1]
-                home_team_spread_odds= main_list[8]
-                over_under=main_list[4]
-                over_under_odds= main_list[5]
-                away_team_money_line = main_list[2]
-                home_team_money_line = main_list[9]
-
-                if away_team_spread > 0 :
-                    away_team_spread = "+%s"%away_team_spread
-                
-                if home_team_money_line > 0 :
-                    home_team_money_line = "+%s"%home_team_money_line
-
-                #string to time object
-                time_object = datetime.strptime(game_time, '%I:%M %p').time()
-                #only games that the time hasn't passed will be available for now
-                if time_object > datetime.now().time():
-                    #print("%s" %game_time)
-                    game_string = "%s %s\t%s(%s)\t%s\t%s" %(away_team_code,away_team_name, away_team_spread,away_team_spread_odds, away_team_money_line, over_under)
-                    vs_string = "%s %s\t%s\t\t%s\t%s" %(home_team_code, home_team_name,home_team_spread_odds,home_team_money_line,over_under_odds)
-
-                    data+="%s\n%s\n%s\n%s\n"%(game_time,game_string,vs_string,dash_sign)
-    return data
